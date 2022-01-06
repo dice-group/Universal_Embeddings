886c8612b5edb926441f02ba0d4eacd138faacab
Adrian Wilke <adibaba@users.noreply.github.com>
2022-01-06 17:06:18 +0100

    KgEmbeddingService (dev, service has to be fixed first: GET->POST)

--- kg-embedding-service.py
+++ kg-embedding-service.py
@@ -2,6 +2,9 @@ import httpx
 import json
 
 # dev version!
+# https://github.com/dice-group/kg-embedding-service
+# https://www.python-httpx.org/
+# https://docs.python.org/3/library/json.html
 
 class KgEmbeddingService():
 
@@ -9,8 +12,30 @@ class KgEmbeddingService():
         self.webservice_url = webservice_url
 
     def get_index_list(self):
-        r = httpx.get(self.webservice_url + '/get-index-list')
-        data = json.loads(r.text)
-        return data['index_list']
+        return json.loads(httpx.get(self.webservice_url + '/get-index-list').text)['index_list']
 
-print(KgEmbeddingService().get_index_list())
+    def get_entity_embedding(self, indexname, entities):
+        # POST is not allowed (405 Method Not Allowed)
+        # GET can not contain JSON (see https://stackoverflow.com/a/983458)
+        # return httpx.post(self.webservice_url + '/get-entity-embedding', json={'indexname':indexname,'entities':entities}).text
+        # TODO: Example with different entities
+        raise NotImplementedError
+
+    def get_entity_embedding_neighbour(self):
+        # /get-entity-embedding-neighbour
+        raise NotImplementedError
+
+    def get_entity_neighbour(self):
+        # /get-entity-neighbour
+        raise NotImplementedError
+
+    def get_index_info(self, indexname):
+        return httpx.post(self.webservice_url + '/get-index-info', json={'indexname':indexname}).text
+
+
+#print(KgEmbeddingService().get_index_list())
+
+# Not working now
+#print(KgEmbeddingService().get_entity_embedding('shallom_dbpedia_index', '/resource/Boeing_747_hull_losses'))
+
+#print(KgEmbeddingService().get_index_info('shallom_dbpedia_index'))
